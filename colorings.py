3fcc6e7a644bd680256aba7b5a12c92005ccf0b6
Bryan Clair <bryan@slu.edu>
2025-11-25 02:26:01 -0600

    Adding graph stats, better interface

--- colorings.py
+++ colorings.py
@@ -8,24 +8,46 @@ Draws all solutions satisfying constraints.
 
 import sys
 import os
+import argparse
 import matplotlib.pyplot as plt
 import networkx as nx
 import colortrade_tools as ct
+from collections import Counter
 
-g = ct.EdgeColoringInstance.from_json(sys.argv[1])
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process a graph file.")
+    parser.add_argument("filename",
+                        help="Input filename")
+    parser.add_argument("-showall",
+                        action="store_true",
+                        help="Show all colorings")
+    return parser.parse_args()
+args = parse_args()
+
+g = ct.EdgeColoringInstance.from_json(args.filename)
 sols = g.solve()
+tg = ct.build_trade_graph(sols)
 
-print(f"Found {len(sols)} colorings:")
-for sol in sols:
-    print(sol)
+print("Color trade graph")
+print("Total colorings (= # of vertices):", len(sols))
+print("Total swaps (= # of edges):", tg.number_of_edges())
+sizes = sorted((len(c) for c in nx.connected_components(tg)), reverse=True)
+print("Number of components:", len(sizes))
+print("Component sizes:", sizes)
+
+print("Degree spectrum")
+degs = [d for _, d in tg.degree()]
+spectrum = Counter(degs)
+for degree in sorted(spectrum):
+    print(degree, ":", spectrum[degree])
 
 # Display solutions
-g.draw_colorings(sols)
-plt.gcf().canvas.manager.set_window_title("All colorings")
-plt.show(block=False)
+if args.showall:
+    g.draw_colorings(sols)
+    plt.gcf().canvas.manager.set_window_title("All colorings")
+    plt.show(block=False)
 
 # Find the color trade graph and show it
-tg = ct.build_trade_graph(sols)
 plt.figure(num = "Color trade graph")
 nx.draw(tg, with_labels=True)
 plt.show()
